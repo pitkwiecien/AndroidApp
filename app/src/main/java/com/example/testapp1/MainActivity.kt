5ee9d6360213dfc3822c6533e9c1e93f8929e037
pitkwiecien <pitkwiecien@gmail.com>
2022-09-23 10:16:47 +0200

    initial commit

--- app/src/main/java/com/example/testapp1/MainActivity.kt
+++ app/src/main/java/com/example/testapp1/MainActivity.kt
@@ -10,10 +10,13 @@ import com.example.testapp1.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var counter: Int = 0
-    private var step: Int = 0
+    private var step: Int = 1
+    private var cancelNext = false
+
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
+        var lastToast = Toast(applicationContext)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -22,19 +25,28 @@ class MainActivity : AppCompatActivity() {
         binding.incrementBtn.setOnClickListener{
             counter += step
             binding.counterValue.text = counter.toString()
-            Toast.makeText(applicationContext, (counter - step).toString() + " + " + step + " = " + counter, Toast.LENGTH_LONG).show()
+            val text = (counter - step).toString() + " + " + step + " = " + counter
+            if(cancelNext) lastToast.cancel()
+            lastToast = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
+            lastToast.show()
+            cancelNext = true
         }
 
         binding.decrementBtn.setOnClickListener{
             counter -= step
             binding.counterValue.text = counter.toString()
-            Toast.makeText(applicationContext, (counter - step).toString() + " + " + step + " = " + counter, Toast.LENGTH_LONG).show()
+            val text = (counter - step).toString() + " - " + step + " = " + counter
+            if(cancelNext) lastToast.cancel()
+            lastToast = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
+            lastToast.show()
+            cancelNext = true
         }
 
         binding.counterStep.addTextChangedListener{
-            if(binding.counterStep.text.length == 0){
+            if(binding.counterStep.text.isEmpty()){
                 binding.incrementBtn.text = "INCREMENT"
                 binding.decrementBtn.text = "DECREMENT"
+                step = 1
             } else {
                 step = binding.counterStep.text.toString().toInt()
                 binding.incrementBtn.text = "+$step"
